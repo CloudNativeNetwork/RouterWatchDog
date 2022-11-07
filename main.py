e4d45a6c5eb86cd8f7489d247b2d1538c3e6c222
Ming Liu <abuxliu@gmail.com>
2022-11-08 01:02:37 +0800

    Upgrade config file.

--- main.py
+++ main.py
@@ -1,35 +1,41 @@
-from config import conf
 import re
+import yaml
 import requests
 from time import sleep
 from ping3 import ping
 
 
-def reboot():
+def reboot(conf):
     session = requests.session()
-    login = session.post(url=conf.ONU_LOGIN_URL, data=conf.ONU_LOGIN_POST_DATA)
+    login = session.post(url="{}/ctlogin.cmd".format(conf['gateway']['url']), data={
+        'username': conf['gateway']['username'],
+        'password': conf['gateway']['password'],
+    })
     if login.status_code == 200:
-        resetrouter = session.get(url="http://192.168.125.1/resetrouter.html")
+        resetrouter = session.get(
+            url="{}/resetrouter.html".format(conf['gateway']['url']))
         if resetrouter.status_code == 200:
-            find_session_key = re.findall(
+            SessionKey = re.findall(
                 'sessionKey=(.*[0-9])', resetrouter.text)[0][1:]
-            if find_session_key == '':
-                print('Cannot Get Session Key.')
-            else:
-                print('sessionKey: ' + find_session_key)
+            print('Session Key: {}.'.format(SessionKey))
         else:
             print("Get Session Key Failed.")
     else:
         print("Login Failed.")
-    reboot = session.get(url=conf.ONU_REBOOT_URL.format(
-        key=find_session_key), headers=conf.HEADERS)
-    print('Reboot Status:' + str(reboot.status_code))
+    reboot = session.get(
+        url="{}/rebootinfo.cgi?sessionKey={}".format(
+            conf['gateway']['url'], SessionKey),
+        headers={
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36',
+        }
+    )
+    print('Reboot Status: {}.'.format(reboot.status_code))
 
 
-def probe(Locations):
+def probe(conf):
     ErrorCount = 0
-    for Location in Locations:
-        sleep(10)
+    for Location in conf['probe']['locations']:
+        sleep(conf['probe']['time'])
         Result = ping(Location)
         if Result is False:
             ErrorCount += 1
@@ -40,7 +46,13 @@ def probe(Locations):
         return True
 
 
+def GetConf(ConfigFilePath):
+    with open(ConfigFilePath, 'r', encoding='utf-8') as f:
+        return yaml.load(f.read(), Loader=yaml.Loader)
+
+
 if __name__ == '__main__':
+    conf = GetConf("config/config.yaml")
     while True:
-        if not probe(Locations=conf.PING):
-            reboot()
+        if not probe(conf):
+            reboot(conf)
